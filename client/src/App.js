c41eb16cc5fb8f52633d290727461cc78ec830c8
Your Name <you@example.com>
2020-09-02 22:15:50 -0700

    updated app

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 import Join from './components/Join/Join';
 import Chat from './components/Chat/Chat';
 
 const App = () => (
     <Router>
-        <Route path="/room/:roomName" exact component={Join}/>
-        <Route path="/chat" exact component={Chat}/>
+        <Switch>
+            <Route path="/room/:roomName" exact component={Join}/>
+            <Route path="/chat" exact component={Chat}/>
+        </Switch>
     </Router>
 );
 
